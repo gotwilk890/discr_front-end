788c5ec9b23c80171c088cdc9b40fc5f9a558c46
gotwilk890 <dave.wilk890@gmail.com>
2015-12-15 13:36:00 -0500

    Adds auth.js file
    Has ajax object which will be used to make calls to the back end.

---
+++ js/authAPI.js
@@ -0,0 +1,51 @@
+'use strict'
+
+var authAPI = {
+
+  api_url: 'https://localhost:3000',
+
+  ajax: function(config, cb){
+    $.ajaxSetup({
+      xhrFields: {
+        withCredentials: true
+      }
+    });
+    $.ajax(config).done(function(data, textStatus, jqhxr){
+      cb(null, data);
+    }).fail(function(jqhxr, status, error){
+      cb({jqhxr: jqhxr, statur: status, error: error});
+    });
+  },
+
+  register: function(credentials, callback){
+    this.ajax({
+      method: 'POST',
+      url: this.api_url +'/signup',
+      contentType:'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  login: function(credentials, callback){
+    this.ajax({
+      method: 'POST',
+      url: this.api_url +'/login',
+      contentType:'application/json; charset=utf-8',
+      data: JSON.stringify(credentials),
+      dataType: 'json'
+    }, callback);
+  },
+
+  logout: function(callback){
+    this.ajax({
+      method: 'POST',
+      url: this.api_url+'/logout',
+      contentType:'application/json; charset=utf-8',
+      data: JSON.stringify({}),
+      dataType: 'json'
+    }, callback);
+  }
+};
+
+
