8c9edb49065ef6d59f277f3618ecae499a709e7e
gotwilk890 <dave.wilk890@gmail.com>
2015-12-15 13:40:48 -0500

    Adds a function to take the form data and mold it into an objec for back end requests.

--- js/authAPI.js
+++ js/authAPI.js
@@ -48,4 +48,13 @@ var authAPI = {
   }
 };
 
-
+var form2object = function(form) {
+  var data = {};
+  $(form).find("input").each(function(index, element) {
+    var type = $(this).attr('type');
+    if ($(this).attr('name') && type !== 'submit' && type !== 'hidden') {
+      data[$(this).attr('name')] = $(this).val();
+    }
+  });
+  return data;
+};
